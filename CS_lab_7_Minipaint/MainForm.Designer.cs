523e09089ec49e952926e3a4d585feb167913df7
IlyaToritsyn <toritsyn41@gmail.com>
2020-06-15 00:33:06 +0300

    Добавьте файлы проекта.

---
+++ CS_lab_7_Minipaint/MainForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace CS_lab_7_Minipaint
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPainting = new System.Windows.Forms.Panel();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.labelColorText = new System.Windows.Forms.Label();
+            this.labelColorImage = new System.Windows.Forms.Label();
+            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.comboBoxFigure = new System.Windows.Forms.ComboBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelPainting
+            // 
+            this.panelPainting.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelPainting.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelPainting.Location = new System.Drawing.Point(122, 12);
+            this.panelPainting.Name = "panelPainting";
+            this.panelPainting.Size = new System.Drawing.Size(666, 426);
+            this.panelPainting.TabIndex = 0;
+            this.panelPainting.Paint += new System.Windows.Forms.PaintEventHandler(this.panelPainting_Paint);
+            this.panelPainting.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelPainting_MouseDown);
+            this.panelPainting.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelPainting_MouseMove);
+            this.panelPainting.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelPainting_MouseUp);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(13, 351);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(103, 87);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // labelColorText
+            // 
+            this.labelColorText.AutoSize = true;
+            this.labelColorText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelColorText.Location = new System.Drawing.Point(40, 12);
+            this.labelColorText.Name = "labelColorText";
+            this.labelColorText.Size = new System.Drawing.Size(41, 17);
+            this.labelColorText.TabIndex = 3;
+            this.labelColorText.Text = "Цвет";
+            // 
+            // labelColorImage
+            // 
+            this.labelColorImage.BackColor = System.Drawing.Color.Black;
+            this.labelColorImage.Location = new System.Drawing.Point(41, 51);
+            this.labelColorImage.Name = "labelColorImage";
+            this.labelColorImage.Size = new System.Drawing.Size(40, 40);
+            this.labelColorImage.TabIndex = 4;
+            this.labelColorImage.Click += new System.EventHandler(this.labelColorImage_Click);
+            // 
+            // numericUpDownWidth
+            // 
+            this.numericUpDownWidth.Location = new System.Drawing.Point(32, 148);
+            this.numericUpDownWidth.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownWidth.Name = "numericUpDownWidth";
+            this.numericUpDownWidth.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDownWidth.TabIndex = 5;
+            this.numericUpDownWidth.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownWidth.ValueChanged += new System.EventHandler(this.numericUpDownWidth_ValueChanged);
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWidth.Location = new System.Drawing.Point(29, 118);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(68, 17);
+            this.labelWidth.TabIndex = 6;
+            this.labelWidth.Text = "Толщина";
+            // 
+            // comboBoxFigure
+            // 
+            this.comboBoxFigure.FormattingEnabled = true;
+            this.comboBoxFigure.Location = new System.Drawing.Point(13, 184);
+            this.comboBoxFigure.Name = "comboBoxFigure";
+            this.comboBoxFigure.Size = new System.Drawing.Size(103, 21);
+            this.comboBoxFigure.Sorted = true;
+            this.comboBoxFigure.TabIndex = 7;
+            this.comboBoxFigure.SelectedIndexChanged += new System.EventHandler(this.comboBoxFigure_SelectedIndexChanged);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(13, 244);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(103, 23);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(13, 273);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(103, 23);
+            this.buttonLoad.TabIndex = 9;
+            this.buttonLoad.Text = "Загрузить";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "(*.xml)|*.xml";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Filter = "(*.xml)|*.xml";
+            // 
+            // colorDialog
+            // 
+            this.colorDialog.AllowFullOpen = false;
+            this.colorDialog.SolidColorOnly = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.comboBoxFigure);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.numericUpDownWidth);
+            this.Controls.Add(this.labelColorImage);
+            this.Controls.Add(this.labelColorText);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.panelPainting);
+            this.MinimumSize = new System.Drawing.Size(816, 488);
+            this.Name = "MainForm";
+            this.Text = "Минипаинт";
+            this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPainting;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelColorText;
+        private System.Windows.Forms.Label labelColorImage;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidth;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.ComboBox comboBoxFigure;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ColorDialog colorDialog;
+    }
+}
+
---
+++ CS_lab_7_Minipaint/MainForm.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace CS_lab_7_Minipaint
+{
+    public partial class MainForm : Form
+    {
+        private bool isMouseButtonPressed = false;
+
+        private Point pointStart;
+        private Point pointCurrent;
+        private Point pointBeforeCurrent;
+
+        private Graphics g;
+
+        private delegate void DrawFigure();
+
+        private DrawFigure figure;
+
+        private string curveLine = "Кривая";
+        private string straightLine = "Прямая";
+        private string rectangle = "Прямоугольник";
+        private string ellipse = "Эллипс";
+
+        private List<int> currentFigure;
+
+        private List<List<int>> content = new List<List<int>>();
+
+        private int x;
+        private int y;
+        private int xDifference; //Длина по оси oX.
+        private int yDifference; //Длина по оси oY.
+
+        private Pen pen;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            comboBoxFigure.Items.AddRange(new object[] {
+            curveLine,
+            straightLine,
+            rectangle,
+            ellipse});
+
+            comboBoxFigure.SelectedIndex = 0;
+            pen = new Pen(labelColorImage.BackColor, (float)numericUpDownWidth.Value);
+            g = panelPainting.CreateGraphics();
+            DoubleBuffered = true; //Умен. мерцания.
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; //Сглаживание.
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round; //Круглое завершение отрезков.
+            figure = DrawCurveLine;
+        }
+
+        /// <summary>
+        /// Если нажат блок выбора цвета.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelColorImage_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelColorImage.BackColor = colorDialog.Color;
+                pen.Color = colorDialog.Color;
+            }
+        }
+
+        /// <summary>
+        /// Если нажата кнопка "Очистить".
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            content.Clear();
+            panelPainting.Refresh();
+        }
+
+        /// <summary>
+        /// Если кнопка мыши отпущена.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panelPainting_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseButtonPressed = false;
+
+            if (figure == DrawStraightLine)
+            {
+                currentFigure.AddRange(new int[] { pointCurrent.X, pointCurrent.Y });
+            }
+
+            else if (figure == DrawRectangle || figure == DrawEllipse)
+            {
+                currentFigure.AddRange(new int[] { x, y, xDifference, yDifference });
+            }
+
+            content.Add(currentFigure);
+        }
+
+        /// <summary>
+        /// Если курсор движется по холсту.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panelPainting_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseButtonPressed)
+            {
+                pointBeforeCurrent = pointCurrent;
+                pointCurrent = e.Location;
+
+                if (figure != DrawCurveLine)
+                {
+                    panelPainting.Invalidate(); //Перерисовка, чтобы видеть, что мы рисуем.
+                }
+
+                figure();
+            }
+        }
+
+        /// <summary>
+        /// Если нажимаем кнопку мыши на холсте.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panelPainting_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseButtonPressed = true;
+            currentFigure = new List<int>();
+            pointStart = e.Location;
+            pointCurrent = e.Location;
+
+            if (figure == DrawCurveLine)
+            {
+                currentFigure.AddRange(new int[] { 0, pen.Color.ToArgb(), (int)pen.Width, pointStart.X, pointStart.Y });
+            }
+
+            else if (figure == DrawStraightLine)
+            {
+                currentFigure.AddRange(new int[] { 1, pen.Color.ToArgb(), (int)pen.Width, pointStart.X, pointStart.Y });
+            }
+
+            else if (figure == DrawRectangle)
+            {
+                currentFigure.AddRange(new int[] { 2, pen.Color.ToArgb(), (int)pen.Width });
+            }
+
+            else if (figure == DrawEllipse)
+            {
+                currentFigure.AddRange(new int[] { 3, pen.Color.ToArgb(), (int)pen.Width });
+            }
+        }
+
+        /// <summary>
+        /// Рисование кривой.
+        /// </summary>
+        private void DrawCurveLine()
+        {
+            g.DrawLine(pen, pointBeforeCurrent, pointCurrent);
+            currentFigure.AddRange(new int[] { pointCurrent.X, pointCurrent.Y });
+        }
+
+        /// <summary>
+        /// Рисование прямой.
+        /// </summary>
+        private void DrawStraightLine()
+        {
+            g.DrawLine(pen, pointStart, pointCurrent);
+        }
+
+        /// <summary>
+        /// Рисование прямоугольника.
+        /// </summary>
+        private void DrawRectangle()
+        {
+            xDifference = pointCurrent.X - pointStart.X;
+            yDifference = pointCurrent.Y - pointStart.Y;
+
+            //Если потянули при рисовании прямоугольника влево.
+            if (xDifference < 0)
+            {
+                x = pointCurrent.X;
+                xDifference = -xDifference;
+            }
+
+            //Если вправо.
+            else
+            {
+                x = pointStart.X;
+            }
+
+            //Если потянули при рисовании прямоугльника вверх.
+            if (yDifference < 0)
+            {
+                y = pointCurrent.Y;
+                yDifference = -yDifference;
+            }
+
+            //Если вниз.
+            else
+            {
+                y = pointStart.Y;
+            }
+
+            g.DrawRectangle(pen, x, y, xDifference, yDifference);
+        }
+
+        /// <summary>
+        /// Рисование эллипса.
+        /// </summary>
+        private void DrawEllipse()
+        {
+            xDifference = pointCurrent.X - pointStart.X;
+            yDifference = pointCurrent.Y - pointStart.Y;
+
+            //Если потянули при рисовании эллипса влево.
+            if (xDifference < 0)
+            {
+                x = pointCurrent.X;
+                xDifference = -xDifference;
+            }
+
+            //Если вправо.
+            else
+            {
+                x = pointStart.X;
+            }
+
+            //Если потянули при рисовании эллипса вверх.
+            if (yDifference < 0)
+            {
+                y = pointCurrent.Y;
+                yDifference = -yDifference;
+            }
+
+            //Если вниз.
+            else
+            {
+                y = pointStart.Y;
+            }
+
+            g.DrawEllipse(pen, x, y, xDifference, yDifference);
+        }
+
+        /// <summary>
+        /// Если меняем фигуру.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBoxFigure_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxFigure.SelectedItem.ToString() == curveLine)
+            {
+                figure = DrawCurveLine;
+            }
+
+            else if (comboBoxFigure.SelectedItem.ToString() == straightLine)
+            {
+                figure = DrawStraightLine;
+            }
+
+            else if (comboBoxFigure.SelectedItem.ToString() == rectangle)
+            {
+                figure = DrawRectangle;
+            }
+
+            else if (comboBoxFigure.SelectedItem.ToString() == ellipse)
+            {
+                figure = DrawEllipse;
+            }
+        }
+
+        /// <summary>
+        /// Перерисовка холста.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panelPainting_Paint(object sender, PaintEventArgs e)
+        {
+            //Перебираем все нарисованные фигуры.
+            foreach (List<int> drawnFigure in content)
+            {
+                //Если фигура - кривая.
+                if (drawnFigure[0] == 0)
+                {
+                    pen.Color = Color.FromArgb(drawnFigure[1]);
+                    pen.Width = drawnFigure[2];
+
+                    for (int i = 3; i < drawnFigure.Count - 2; i += 2)
+                    {
+                        g.DrawLine(pen, drawnFigure[i], drawnFigure[i + 1], drawnFigure[i + 2], drawnFigure[i + 3]);
+                    }
+                }
+
+                //Если фигура - прямая.
+                else if (drawnFigure[0] == 1)
+                {
+                    pen.Color = Color.FromArgb(drawnFigure[1]);
+                    pen.Width = drawnFigure[2];
+
+                    g.DrawLine(pen, drawnFigure[3], drawnFigure[4], drawnFigure[5], drawnFigure[6]);
+                }
+
+                //Если фигура - прямоугольник.
+                else if (drawnFigure[0] == 2)
+                {
+                    pen.Color = Color.FromArgb(drawnFigure[1]);
+                    pen.Width = drawnFigure[2];
+
+                    g.DrawRectangle(pen, drawnFigure[3], drawnFigure[4], drawnFigure[5], drawnFigure[6]);
+                }
+
+                //Если фигура - эллипс.
+                else if (drawnFigure[0] == 3)
+                {
+                    pen.Color = Color.FromArgb(drawnFigure[1]);
+                    pen.Width = drawnFigure[2];
+
+                    g.DrawEllipse(pen, drawnFigure[3], drawnFigure[4], drawnFigure[5], drawnFigure[6]);
+                }
+            }
+
+            //Получаем настройки кисти из панели инструментов.
+            pen.Color = labelColorImage.BackColor;
+            pen.Width = (float)numericUpDownWidth.Value;
+
+            //Если мы рисуем во время перерисовки холста.
+            if (isMouseButtonPressed)
+            {
+                if (figure == DrawStraightLine)
+                {
+                    g.DrawLine(pen, pointStart, pointCurrent);
+                }
+
+                else if (figure == DrawRectangle)
+                {
+                    g.DrawRectangle(pen, x, y, xDifference, yDifference);
+                }
+
+                else if (figure == DrawEllipse)
+                {
+                    g.DrawEllipse(pen, x, y, xDifference, yDifference);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Если изменён размер формы.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_SizeChanged(object sender, EventArgs e)
+        {
+            g.Dispose();
+
+            g = panelPainting.CreateGraphics();
+            DoubleBuffered = true; //Умен. мерцания.
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; //Сглаживание.
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round; //Круглое завершение отрезков.
+
+            Refresh(); //Перерисовка со сглаживанием.
+        }
+
+        /// <summary>
+        /// Если меняем толщину.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void numericUpDownWidth_ValueChanged(object sender, EventArgs e)
+        {
+            pen.Width = (float)numericUpDownWidth.Value;
+        }
+
+        /// <summary>
+        /// Если нажата кнопка "Сохранить".
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                XmlSerializer formatter = new XmlSerializer(typeof(List<List<int>>));
+
+                using (FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    formatter.Serialize(fs, content);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Если нажата кнопка "Загрузить".
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                content.Clear();
+
+                XmlSerializer formatter = new XmlSerializer(typeof(List<List<int>>));
+
+                using (FileStream fs = new FileStream(openFileDialog.FileName, FileMode.Open))
+                {
+                    content = (List<List<int>>)formatter.Deserialize(fs);
+                }
+
+                panelPainting.Refresh();
+            }
+        }
+    }
+}
